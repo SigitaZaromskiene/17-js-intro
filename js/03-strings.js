a136a77b96ba1116f3d060632204b5564301fdba
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2022-12-08 13:05:59 +0200

    Homework

--- js/03-strings.js
+++ js/03-strings.js
@@ -1,1 +1,41 @@
 console.log("String");
+
+const number1 = 1;
+console.log(number1);
+const number2 = 2;
+console.log(number2);
+const number3 = 3;
+console.log(number3);
+
+const result = number1 + number2 + number3;
+console.log(result);
+
+const firstName = "Sigita";
+console.log(firstName);
+const lastName = "Zaromskiene";
+console.log(lastName);
+const age = 33;
+console.log(age);
+
+const sentance = `I am ${firstName} ${lastName} and I am ${age} years old`;
+const sentance1 =
+  "I" +
+  " " +
+  "am" +
+  " " +
+  firstName +
+  " " +
+  lastName +
+  " " +
+  "and" +
+  " " +
+  "I" +
+  " " +
+  "am" +
+  " " +
+  age +
+  " " +
+  "years" +
+  " " +
+  "old";
+console.log(sentance, sentance1);
