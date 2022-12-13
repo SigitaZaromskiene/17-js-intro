cf38882daea0fbf9f28f03ea2ad38933fa89f2ef
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2022-12-13 13:10:02 +0200

    Array homework

--- js/05-array.js
+++ js/05-array.js
@@ -1,1 +1,28 @@
 console.log("array");
+
+// ------------- ND-----------------
+
+const fruits = ["apples", "oranges", "bananas", "pears", "limes"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits[4]);
+
+const sports = ["basketball", "football", "golf", "hokey", "jogging"];
+console.log(sports[0], sports[1], sports[2], sports[3], sports[4]);
+
+const animals = ["dogs", "monkeys", "lions", "cats", "tigers"];
+
+animals.map((animal) => {
+  console.log(animal);
+});
+
+const expenses = [22, 5, 6, 87, 5];
+console.log(expenses[0], expenses[1], expenses[2], expenses[3], expenses[4]);
+
+const tips = [2, 3, 6, 8, 1];
+console.log(tips);
+
+const days = [6, 7, 16, 22, 29];
+console.log(days);
