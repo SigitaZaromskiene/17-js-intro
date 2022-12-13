30b99eff9e41392665d252e9c5cd323090883dca
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2022-12-13 12:52:35 +0200

    if/functions

---
+++ js/07-object.js
@@ -0,0 +1,34 @@
+// const student1 = {
+//   name: "Jonas",
+//   age: 99,
+//   isMarried: false,
+//   marks: [],
+// };
+// console.log(student1);
+
+// const student2 = {
+//   age: 69,
+//   isMarried: true,
+//   name: "Ona",
+//   marks: [10, 2, 8, 4, 6],
+// };
+// console.log(student2);
+
+// // STANDARTINIS REIKSMES ISTRAUKIMAS IS OBJEKTO -> obj['key']
+// const stud1Name = student1["name"];
+// const stud1Age = student1["age"];
+// const stud1Marks = student1["marks"];
+// const stud1Status = student1["isMarried"];
+// console.log(stud1Name);
+// console.log(stud1Age);
+// console.log(stud1Marks);
+// console.log(stud1Status);
+
+// const stud2Name = student2["name"];
+// const stud2Age = student2["age"];
+// const stud2Marks = student2["marks"];
+// const stud2Status = student2["isMarried"];
+// console.log(stud2Name);
+// console.log(stud2Age);
+// console.log(stud2Marks);
+// console.log(stud2Status);
