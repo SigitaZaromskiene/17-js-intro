e7c5a32b75aec905a4b71b6a64cb5319066d924d
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2022-12-22 15:07:30 +0200

    Sort

---
+++ js/20-sort.js
@@ -0,0 +1,11 @@
+const dic = ["Petras", "Maryte", "Ona"];
+
+const newDic = [];
+const n = dic.sort();
+console.log(newDic);
+console.log(n);
+
+const num = [1, 2, 15, 2, 47];
+
+const minus = num.sort((a, b) => a - b);
+console.log(minus);
